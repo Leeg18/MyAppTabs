5a86ba215106721597761dfde76b84cfb81bae42
Leeg18 <Leo.Sormunen@student.laurea.fi>
2019-10-07 20:06:42 +0300

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  { path: 'pagename', loadChildren: './pagename/pagename.module#PAGENAMEPageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
